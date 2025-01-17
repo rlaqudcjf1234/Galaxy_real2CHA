74a109f417e376ada4a363cdb52b2ccb7fe44fc6
hjs7186 <wnstjq135@gmail.com>
2025-01-17 12:08:08 +0900

    충돌해결

--- src/main/web/src/App.jsx
+++ src/main/web/src/App.jsx
@@ -1,33 +1,36 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { PostProvider } from "./contexts/PostContext";
 
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Container from './components/Container';
-import AdminList from './pages/admin/List';
-import AdminAdd from './pages/admin/Add';
-import Notice from './pages/student/Notice';
-import Counsel from './pages/student/Counsel';
-import Lecture_Plan from './pages/student/Lecture_Plan';
-import Apply from './pages/student/Apply';
-import Lec_Notice from './pages/student/Lec_Notice';
-import Chulseog from './pages/student/Chulseog';
-import Qualification_Info from './pages/student/Qualification_Info';
-import Timetable from './pages/student/Timetable';
-import Stats_Analysis from './pages/student/Stats_Analysis';
-import Aftercare_Apply from './pages/student/Aftercare_Apply';
-import Aftercare_Progress from './pages/student/Aftercare_Progress';
-import Aftercare_Results from './pages/student/Aftercare_Results';
-import Survey_YN from './pages/student/Survey_YN';
-import Admin_Survey from './pages/student/Admin_Survey';
-import Survey_Results from './pages/student/Survey_Results';
-import Class_Community from './pages/student/Class_Community';
-import Community_Notice from './pages/student/Community_Notice';
-import QNA from './pages/student/QNA';
-import WritePost from './pages/student/WritePost';
-import {PostProvider} from './contexts/PostContext';
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Container from "./components/Container";
+import AdminList from "./pages/admin/List";
+import AdminAdd from "./pages/admin/Add";
+import Notice from "./pages/student/Notice";
+import Counsel from "./pages/student/Counsel";
+import Lecture_Plan from "./pages/student/Lecture_Plan";
+import Apply from "./pages/student/Apply";
+import Lec_Notice from "./pages/student/Lec_Notice";
+import Chulseog from "./pages/student/Chulseog";
+import Qualification_Info from "./pages/student/Qualification_Info";
+import Timetable from "./pages/student/Timetable";
+import Stats_Analysis from "./pages/student/Stats_Analysis";
+import Aftercare_Apply from "./pages/student/Aftercare_Apply";
+import Aftercare_Progress from "./pages/student/Aftercare_Progress";
+import Aftercare_Results from "./pages/student/Aftercare_Results";
+import Survey_YN from "./pages/student/Survey_YN";
+import Admin_Survey from "./pages/student/Admin_Survey";
+import Survey_Results from "./pages/student/Survey_Results";
+import Class_Community from "./pages/student/Class_Community";
+import Community_Notice from "./pages/student/Community_Notice";
+import QNA from "./pages/student/QNA";
+import WritePost from "./pages/student/WritePost";
+import ApplyAdmReg from "./pages/apply/ApplyAdmReg";
+import ApplyStdReg from "./pages/apply/ApplyStdReg";
+import ApplyList from "./pages/apply/ApplyList";
 
 function App() {
     return (
@@ -35,58 +38,56 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<Layout />}>
-
-                        <Route index="index" element={<Home />}/>
+                        <Route index="index" element={<Home />} />
 
                         <Route path="admin" element={<Container />}>
-                            <Route index="index" element={<AdminList />}/>
-                            <Route path="add" element={<AdminAdd />}/>
+                            <Route index="index" element={<AdminList />} />
+                            <Route path="add" element={<AdminAdd />} />
                         </Route>
-                        
+
                         <Route path="lecture" element={<Container />}>
-                            <Route /*index*/ path="list"/>
-                            <Route path="add"/>
+                            <Route /*index*/ path="list" />
+                            <Route path="add" />
                         </Route>
-                        
+
                         <Route path="class" element={<Container />}>
-                            <Route /*index*/ path="list"/>
-                            <Route path="add"/>
-                            <Route path="read"/>
-                            <Route path="confirm"/>
+                            <Route /*index*/ path="list" />
+                            <Route path="add" />
+                            <Route path="read" />
+                            <Route path="confirm" />
                         </Route>
-                        
+
                         <Route path="apply" element={<Container />}>
-                            <Route /*index*/ path="list"/>
-                            <Route path="read"/>
-                            <Route path="confirm"/>
+                            <Route index="index" element={<ApplyList />} />
+                            <Route path="adm" element={<ApplyAdmReg />} />
+                            <Route path="std" element={<ApplyStdReg />} />
                         </Route>
 
                         <Route path="adminCommunity" element={<Container />}>
-                            <Route /*index*/ path="list"/>
-                            <Route path="add"/>
-                            <Route path="read"/>
-                            <Route path="delete"/>
+                            <Route /*index*/ path="list" />
+                            <Route path="add" />
+                            <Route path="read" />
+                            <Route path="delete" />
                         </Route>
                         <Route path="lectureDocument" element={<Container />}>
-                            <Route /*index*/ path="list"/>
-                            <Route path="add"/>
-                            <Route path="read"/>
-                            <Route path="delete"/>
+                            <Route /*index*/ path="list" />
+                            <Route path="add" />
+                            <Route path="read" />
+                            <Route path="delete" />
                         </Route>
                         <Route path="classComunity" element={<Container />}>
-                            <Route /*index*/ path="list"/>
-                            <Route path="add"/>
-                            <Route path="read"/>
-                            <Route path="delete"/>
+                            <Route /*index*/ path="list" />
+                            <Route path="add" />
+                            <Route path="read" />
+                            <Route path="delete" />
                         </Route>
 
                         <Route path="group" element={<Container />}>
-                            <Route /*index*/ path="list"/>
+                            <Route /*index*/ path="list" />
                         </Route>
                         <Route path="code" element={<Container />}>
-                            <Route /*index*/ path="list"/>
+                            <Route /*index*/ path="list" />
                         </Route>
-
                     </Route>
                 </Routes>
             </Router>
