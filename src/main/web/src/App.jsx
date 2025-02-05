79176f033190debaee55cd8bfbf221bb23f7cc38
kalan61 <did890722@naver.com>
2025-02-05 18:07:57 +0900

    App 통일 수정

--- src/main/web/src/App.jsx
+++ src/main/web/src/App.jsx
@@ -7,28 +7,29 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import Container from './components/Container';
 
+const TempList = lazy(() => import ('./pages/temp/List'));
+const TempAdd = lazy(() => import ('./pages/temp/Add'));
+const TempRead = lazy(() => import ('./pages/temp/Read'));
+
 const AdminList = lazy(() => import ('./pages/admin/List'));
 const AdminAdd = lazy(() => import ('./pages/admin/Add'));
 const AdminMod = lazy(() => import ('./pages/admin/Mod'));
 const AdminPass = lazy(() => import ('./pages/admin/Pass'));
 
-import List from './pages/lecture/list';
-import Add from './pages/lecture/Add';
-import Read from './pages/lecture/Read';
-import Update from "./pages/lecture/Update";
-import LectureDocument from "./pages/lectureDocum/Document";
+const ClassList = lazy(() => import ('./pages/class/List'));
+const ClassAdd = lazy(() => import ('./pages/class/Add'));
+const ClassRead = lazy(() => import ('./pages/class/Read'));
+const ClassConfirm = lazy(() => import ('./pages/class/Confirm'));
+const ClassDetail = lazy(() => import ('./pages/class/Detail'));
 
-import ClassList from './pages/class/List';
-import ClassAdd from './pages/class/Add';
-import ClassRead from './pages/class/Read';
-import ClassConfirm from './pages/class/Confirm';
-import ClassDetail from './pages/class/Detail';
+const LectureList = lazy(() => import ('./pages/lecture/List'));
+const LectureAdd = lazy(() => import ('./pages/lecture/Add'));
+const LectureRead = lazy(() => import ('./pages/lecture/Read'));
+const LectureMod = lazy(() => import ('./pages/lecture/Mod'));
+
+const LectureDocList = lazy(() => import ('./pages/lectureDoc/List'));
 
 import UserLogin from './pages/user/Login';
-
-const TempList = lazy(() => import ('./pages/temp/List'));
-const TempAdd = lazy(() => import ('./pages/temp/Add'));
-const TempRead = lazy(() => import ('./pages/temp/Read'));
 
 function App() {
     return (
@@ -54,19 +55,26 @@ function App() {
                             <Route path="mod/:seq" element={<AdminMod />}/>
                         </Route>
 
-                        <Route path="lecture" element={<Container />}>
-                            <Route index="index" element={<List />}/>
-                            <Route path="read/:id" element={<Read />}/>
-                            <Route path="add" element={<Add />}/>
-                            <Route path="update/:id" element={<Update />}/>
-                        </Route>
-
                         <Route path="class" element={<Container/>}>
                             <Route index="index" element={<ClassList/>}/>
                             <Route path="add" element={<ClassAdd/>}/>
                             <Route path="read" element={<ClassRead/>}/>
                             <Route path="confirm" element={<ClassConfirm/>}/>
                             <Route path="detail/:seq" element={<ClassDetail/>}/>
+                        </Route>
+
+                        <Route path="lecture" element={<Container />}>
+                            <Route index="index" element={<LectureList />}/>
+                            <Route path="add" element={<LectureAdd />}/>
+                            <Route path="read/:id" element={<LectureRead />}/>
+                            <Route path="mod/:id" element={<LectureMod />}/>
+                        </Route>
+
+                        <Route path="lectureDoc" element={<Container />}>
+                            <Route index="index" element={<LectureDocList />}/>
+                            <Route path="add"/>
+                            <Route path="read"/>
+                            <Route path="delete"/>
                         </Route>
 
                         <Route path="apply" element={<Container />}>
@@ -78,13 +86,6 @@ function App() {
                         <Route path="adminCommunity" element={<Container />}>
                             <Route /*index*/
                                 element=""/>
-                            <Route path="add"/>
-                            <Route path="read"/>
-                            <Route path="delete"/>
-                        </Route>
-
-                        <Route path="lectureDocument" element={<Container />}>
-                            <Route index="index" element={<LectureDocument />}/>
                             <Route path="add"/>
                             <Route path="read"/>
                             <Route path="delete"/>
--- src/main/web/src/pages/lecture/Add.jsx
+++ src/main/web/src/pages/lecture/Add.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import '../../css/Add.css';
 
 function Add() {
     const navigate = useNavigate();
