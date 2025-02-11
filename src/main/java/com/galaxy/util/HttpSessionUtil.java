3a267b804ee990d594fe0d0e9686bc8728f4ff7e
kalan61 <did890722@naver.com>
2025-02-11 12:08:43 +0900

    로그인 사용자 seq 획득 함수 util수정

--- src/main/java/com/galaxy/util/HttpSessionUtil.java
+++ src/main/java/com/galaxy/util/HttpSessionUtil.java
@@ -2,25 +2,39 @@ package com.galaxy.util;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 public class HttpSessionUtil {
 
-    @Autowired HttpServletRequest request;
+    public static HttpServletRequest getRequest()throws Exception {
 
-    public String getLoginSeq() {
+        ServletRequestAttributes attr = (ServletRequestAttributes)RequestContextHolder.currentRequestAttributes();
+
+        return attr.getRequest();
+    }
+
+    public static HttpSession getSession()throws Exception {
+
+        HttpServletRequest request = getRequest();
+
+        return request.getSession();
+    }
+
+    public static String getLoginSeq() {
 
         String seq = "";
 
         try {
-            HttpSession session = request.getSession();
 
-            Map<String, Object> user = (Map<String, Object>) session.getAttribute("user");
+            HttpSession session = getSession();
 
-            seq = (String) user.get("SEQ");
+            Map<String, Object> user = (Map<String, Object>)session.getAttribute("user");
+
+            seq = (String)user.get("SEQ");
 
         } catch (Exception e) {
             // TODO: handle exception
@@ -29,5 +43,23 @@ public class HttpSessionUtil {
 
         return seq;
     }
-    
+
+    public String getLoginEmail() {
+
+        String email = "";
+
+        try {
+            HttpSession session = getSession();
+
+            Map<String, Object> user = (Map<String, Object>)session.getAttribute("user");
+
+            email = (String)user.get("EMAIL");
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+        return email;
+    }
 }
