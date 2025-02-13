982f80547fa0bab8115ada066454ccb036e0d565
hjs7186 <wnstjq135@gmail.com>
2025-02-13 15:41:52 +0900

    App.jsx Apply 부분 수정

--- src/main/web/src/App.jsx
+++ src/main/web/src/App.jsx
@@ -3,44 +3,44 @@ import "./App.css";
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Container from './components/Container';
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Container from "./components/Container";
 
-const UserLogin = lazy(() => import('./pages/user/Login'));
+const UserLogin = lazy(() => import("./pages/user/Login"));
 // const UserModify = lazy(() => import('./pages/user/Modify'));
 
-const TempList = lazy(() => import('./pages/temp/List'));
-const TempAdd = lazy(() => import('./pages/temp/Add'));
-const TempRead = lazy(() => import('./pages/temp/Read'));
+const TempList = lazy(() => import("./pages/temp/List"));
+const TempAdd = lazy(() => import("./pages/temp/Add"));
+const TempRead = lazy(() => import("./pages/temp/Read"));
 
-const AdminList = lazy(() => import('./pages/admin/List'));
-const AdminAdd = lazy(() => import('./pages/admin/Add'));
-const AdminMod = lazy(() => import('./pages/admin/Mod'));
-const AdminPass = lazy(() => import('./pages/admin/Pass'));
+const AdminList = lazy(() => import("./pages/admin/List"));
+const AdminAdd = lazy(() => import("./pages/admin/Add"));
+const AdminMod = lazy(() => import("./pages/admin/Mod"));
+const AdminPass = lazy(() => import("./pages/admin/Pass"));
 
-const ClassList = lazy(() => import('./pages/class/List'));
-const ClassAdd = lazy(() => import('./pages/class/Add'));
-const ClassRead = lazy(() => import('./pages/class/Read'));
+const ApplyList = lazy(() => import("./pages/apply/ApplyList"));
+const ApplyAdd = lazy(() => import("./pages/apply/ApplyAdd"));
+const ApplyRead = lazy(() => import("./pages/apply/ApplyRead"));
+const ApplyAdmin = lazy(() => import("./pages/apply/ApplyAdmin"));
+
+const ClassList = lazy(() => import("./pages/class/List"));
+const ClassAdd = lazy(() => import("./pages/class/Add"));
+const ClassRead = lazy(() => import("./pages/class/Read"));
 // const ClassUpdate = lazy(() => import ('./pages/class/Update'));
 
+const LectureList = lazy(() => import("./pages/lecture/List"));
+const LectureAdd = lazy(() => import("./pages/lecture/Add"));
+const LectureRead = lazy(() => import("./pages/lecture/Read"));
+const LectureMod = lazy(() => import("./pages/lecture/Mod"));
 
-const LectureList = lazy(() => import('./pages/lecture/List'));
-const LectureAdd = lazy(() => import('./pages/lecture/Add'));
-const LectureRead = lazy(() => import('./pages/lecture/Read'));
-const LectureMod = lazy(() => import('./pages/lecture/Mod'));
-
-const LectureDocList = lazy(() => import('./pages/lectureDoc/List'));
+const LectureDocList = lazy(() => import("./pages/lectureDoc/List"));
 // const LectureDocAdd = lazy(() => import ('./pages/lectureDoc/Add'));
 // const LectureDocRead = lazy(() => import ('./pages/lectureDoc/Read'));
 // const LectureDocMod = lazy(() => import ('./pages/lectureDoc/Mod'));
 
-const CodeList = lazy(() => import('./pages/codegroup/List'))
-const CodeAdd = lazy(() => import('./pages/codegroup/Add'))
-const CodeRead = lazy(() => import('./pages/codegroup/Read'))
-const CodeMod = lazy(() => import('./pages/codegroup/Mod'))
-
-
+// const CodeList = lazy(()=> import('./pages/codegroup/List'))
+// const CodeAdd = lazy(()=> import('./pages/codegroup/add'))
 
 function App() {
     return (
@@ -83,37 +83,33 @@ function App() {
                         </Route>
 
                         <Route path="apply" element={<Container />}>
-                            <Route index="index" element="" />
-                            <Route path="adm" />
-                            <Route path="std" />
+                            <Route index="index" element={<ApplyList />} />
+                            <Route path="add" element={<ApplyAdd />} />
+                            <Route path="read/:id" element={<ApplyRead />} />
+                            <Route path="admin" element={<ApplyAdmin />} />
                         </Route>
 
                         <Route path="adminCommunity" element={<Container />}>
-                            <Route /*index*/
-                                element="" />
+                            <Route /*index*/ element="" />
                             <Route path="add" />
                             <Route path="read" />
                             <Route path="delete" />
                         </Route>
 
                         <Route path="classComunity" element={<Container />}>
-                            <Route /*index*/
-                                element="" />
+                            <Route /*index*/ element="" />
                             <Route path="add" />
                             <Route path="read" />
                             <Route path="delete" />
                         </Route>
 
                         <Route path="group" element={<Container />}>
-                            <Route index="index" element={<CodeList />} />
-                            <Route path="add" element={<CodeAdd />} />
-                            <Route path="read/:group_id" element={<CodeRead />} />
-                            <Route path="mod/:group_id" element={<CodeMod />} />
+                            {/* <Route index="index" element={<CodeList />} />
+                            <Route path="add" element={<CodeAdd />} /> */}
                         </Route>
 
                         <Route path="code" element={<Container />}>
-                            <Route /*index*/
-                                element="" />
+                            <Route /*index*/ element="" />
                         </Route>
                     </Route>
                     <Route path="/admin/pass/:seq" element={<AdminPass />} />
@@ -123,7 +119,6 @@ function App() {
                         {/* <Route path="modify" element={<UserModify/>}/> */}
                     </Route>
                 </Routes>
-
             </Suspense>
         </Router>
     );
