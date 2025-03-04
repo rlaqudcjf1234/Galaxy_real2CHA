9a8d3a9457f4af223fad87d74bcbf8da2c129925
kalan61 <did890722@naver.com>
2025-03-04 10:02:07 +0900

    로그인 유지 (토큰 저장 환경 변경) 변경

--- src/main/java/com/galaxy/security/RefreshTokenAuthenticationFilter.java
+++ src/main/java/com/galaxy/security/RefreshTokenAuthenticationFilter.java
@@ -15,38 +15,43 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class RefreshTokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {  
-  
-    private static final String REFRESH_URL = "/refresh";  
-    private final JwtService jwtService;  
-  
-    public RefreshTokenAuthenticationFilter(JwtService jwtService) {  
-        super(REFRESH_URL);  
-        this.jwtService = jwtService;  
-    }  
-  
-    @Override  
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {  
-        String refreshToken = jwtService.extractRefreshToken(request)  
-                .orElseThrow(() -> new AuthenticationServiceException("No refresh token provided"));  
-  
-  
-        RefreshTokenAuthenticationToken refreshTokenAuthenticationToken= RefreshTokenAuthenticationToken.unAuthenticated(refreshToken, null);  
-        return this.getAuthenticationManager().authenticate(refreshTokenAuthenticationToken);  
-    }  
-  
-    @Override  
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {  
+public class RefreshTokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    private static final String REFRESH_URL = "/refresh";
+    private final JwtService jwtService;
+
+    public RefreshTokenAuthenticationFilter(JwtService jwtService) {
+        super(REFRESH_URL);
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+            HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        String refreshToken = jwtService.extractRefreshToken(request)
+                .orElseThrow(() -> new AuthenticationServiceException("No refresh token provided"));
+
+
+        RefreshTokenAuthenticationToken refreshTokenAuthenticationToken =
+                RefreshTokenAuthenticationToken.unAuthenticated(refreshToken, null);
+        return this.getAuthenticationManager().authenticate(refreshTokenAuthenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+            HttpServletResponse response, FilterChain chain, Authentication authentication)
+            throws IOException, ServletException {
         User user = (User) authentication.getPrincipal();
         String username = authentication.getName();
-  
-        String accessToken = jwtService.generateAccessToken(user);  
-        String refreshToken = jwtService.generateRefreshToken();  
-  
-        response.setContentType("application/json");  
-        response.setCharacterEncoding("utf-8");  
-  
-        jwtService.setAccessToken(response, accessToken);  
-        jwtService.setRefreshToken(response, refreshToken, username);  
-    }  
+
+        String accessToken = jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken();
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        jwtService.setAccessToken(response, accessToken);
+        jwtService.setRefreshToken(response, refreshToken, username);
+    }
 }
